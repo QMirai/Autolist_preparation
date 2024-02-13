1b6b92449eed35589ef11ba4036dc8767b2a5e27
QMirai <smallqiao4@outlook.com>
2024-02-13 15:35:37 -0500

    first commit

---
+++ batch_grew/batch_grew.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+grew compile -i eng_fre_sud.json
+
+for type in "control_verb" # "raising_verb"
+do
+	input_dir="/mnt/hgfs/share/list_fre_${type}"
+	echo "\n-----$type-----"
+	output_dir="/mnt/hgfs/share/output_${input_dir##*/}"
+	mkdir -p "$output_dir"
+
+	for file in "$input_dir"/*.req
+	do
+		echo "Processing $file"
+		filename=$(basename -- "$file")
+		filename="${filename%.*}"
+		grew grep -request $file -i eng_fre_sud.json > "$output_dir"/output_sud_"$filename".json
+	done
+done
+
+echo "Finished!"
