16d7da0410cfe1c284c2a1bcf5d40730f15f7351
QMirai <smallqiao4@outlook.com>
2024-02-28 15:25:40 -0500

    batch with if statement; updated req

--- batch_grew/batch_grew.sh
+++ batch_grew/batch_grew.sh
@@ -2,21 +2,38 @@
 
 grew compile -i eng_fre_sud.json
 
-for type in "raising_verb" # "control_verb"
-do
-	lang="eng"
-	input_dir="/mnt/hgfs/share/req/req_${lang}_${type}"
-	echo "\n-----$type-----"
-	output_dir="/mnt/hgfs/share/grew_output/grew_output_${input_dir##*/req_}"
-	mkdir -p "$output_dir"
+echo "re, rf, ce, cf"
+read v_l
 
-	for file in "$input_dir"/*.req
-	do
-		echo "Processing $file"
-		filename=$(basename -- "$file")
-		filename="${filename%.*}"
-		grew grep -request $file -i eng_fre_sud.json > "$output_dir"/output_sud_"$filename".json
-	done
+if [ "$v_l"=="re" ]; then
+	lang="eng"
+	v_type="raising"
+elif [ "$v_l"=="rf" ]; then
+    	lang="fre"
+    	v_type="raising"
+elif [ "$v_l"=="ce" ]; then
+    	lang="eng"
+    	v_type="control"
+elif [ "$v_l"=="cf" ]; then
+    	lang="fre"
+    	v_type="control"
+else
+	echo "$v_l not accepted"
+	exit 1
+fi
+	
+input_dir="/mnt/hgfs/share/req/req_${lang}_${v_type}_verb"
+echo "\n----- ($lang) $v_type verb-----"
+output_dir="/mnt/hgfs/share/grew_output/grew_output_${input_dir##*/req_}"
+mkdir -p "$output_dir"
+
+for file in "$input_dir"/*.req
+do
+	echo "Processing $file"
+	filename=$(basename -- "$file")
+	filename="${filename%.*}"
+	grew grep -request $file -i eng_fre_sud.json > "$output_dir"/output_sud_"$filename".json
 done
+
 
 echo "Finished!"
